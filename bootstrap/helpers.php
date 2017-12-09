d6d721ae39d31ac64c60745c22e484cd4d95defd
machpng <macp.mcp@gmail.com>
2017-12-09 16:29:18 +0800

    update

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -5,3 +5,8 @@
  * Date: 2017/12/9
  * Time: 上午12:01
  */
+
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://weibo.com/2900250937/profile?rightmod=1&wvr=6&mod=personinfo&is_all=1#_0" target="_blank">Machpng</a> 设计和编码
+            <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>
