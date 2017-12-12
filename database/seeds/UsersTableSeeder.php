d8af99f1ed5478e17ed689ed384a523a6c2539df
machpng <macp.mcp@gmail.com>
2017-12-12 08:03:15 +0000

    Seeding topics and users data

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        // 头像假数据
+        $avatars = [
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
+        ];
+
+        // 生成数据集合
+        $users = factory(User::class)
+            ->times(10)->make()
+            ->each(function ($user, $index) use ($faker, $avatars) {
+                $user->avatar = $faker->randomElement($avatars);
+            });
+
+        // 让隐藏字段可见，并将书局集合转换成数组
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        // 插入到数据库中
+        User::insert($user_array);
+
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'machpng';
+        $user->email = '416007060@qq.com';
+        $user->avatar = 'http://larabbs.app/uploads/images/avatars/201712/12//2_1513045735_axOpB4WVlj.jpg';
+        $user->introduction = 'Just Do it.';
+        $user->save();
+    }
+}
