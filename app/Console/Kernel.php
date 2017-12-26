75e958fdc06076115707eed3139ba35cad3e6c84
vagrant <vagrant@homestead>
2017-12-26 06:43:06 +0000

    Keed track of user last visited time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-       $schedule->command('larabbs:calculate-active-user')->hourly();
+        // 每隔一个小时执行一遍
+        $schedule->command('larabbs:calculate-active-user')->hourly();
+
+        // 每日零时执行一次
+        $schedule->command('larabbs:sync-user-actived_at')->dailyAt('00:00');
     }
 
     /**
